d7e310bdc9429537630c7be27197b05e37a9f239
Anders Wallin <anders.wallin@windriver.com>
2020-04-27 21:34:21 +0200

    added SSTATE_MIRROR

---
+++ hostconfig-arn-awallin-linux-l5.mk
@@ -0,0 +1,27 @@
+# arn-awallin-linux-l5 host config file
+#
+SSTATE_SSHFS_REMOTE	?= ab3:/opt/awallin/src/build_wrlinux18/out_lts18.16/sstate-cache
+SSTATE_SSHFS_MOUNT	?= $(TOP)/sstate_mirror_mount
+SSTATE_MIRROR_URL 	?= file://$(SSTATE_SSHFS_MOUNT)
+
+sshfs.mount:
+	$(TRACE)
+	$(MKDIR) $(SSTATE_SSHFS_MOUNT)
+	$(Q)mountpoint $(SSTATE_SSHFS_MOUNT) || sshfs $(SSTATE_SSHFS_REMOTE) $(SSTATE_SSHFS_MOUNT)
+
+sshfs.umount:
+	$(TRACE)
+	-$(Q)mountpoint $(SSTATE_SSHFS_MOUNT) && fusermount -u $(SSTATE_SSHFS_MOUNT)
+
+$(HOSTNAME).configure: sshfs.mount # setup remote SSTATE_MIRROR
+	$(TRACE)
+	$(eval localconf=$(BUILDDIR)/conf/local.conf)
+	$(GREP) -q "^SSTATE_MIRRORS" $(localconf) || \
+		echo "SSTATE_MIRRORS = \"file://.* $(SSTATE_MIRROR_URL)/PATH\"" >> $(localconf)
+	$(GREP) -q "^SSTATE_MIRROR_ALLOW_NETWORK" $(localconf) || \
+		echo "SSTATE_MIRROR_ALLOW_NETWORK = \"1\"" >> $(localconf)
+
+$(HOSTNAME).unconfigure: sshfs.umount
+	$(TRACE)
+
+configure:: $(HOSTNAME).configure
